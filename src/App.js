44051992b13f8e5bf86be8e4ecbf9a623273e790
DESKTOP-5AOR58F\Rishi <3rishabh3@gmail.com>
2021-05-18 22:58:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./Components/NavBar";
+import Banner from "./Components/Banner";
+import Row from "./Components/Row";
+import requests from "./Services/requests";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <NavBar />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        url={requests.fetchNetflixOriginals}
+        isRowLarge
+      />
+      <Row title="Trending" url={requests.fetchTrending} />
+      <Row title="Top Rated" url={requests.fetchTopRated} />
+      <Row title="Action Movies" url={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" url={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" url={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" url={requests.fetchRomanceMovies} />
+      <Row title="Documentries" url={requests.fetchDocumentries} />
+      <p
+        style={{ textAlign: "center", color: "#cf412e", paddingBottom: "10px" }}
+      >
+        ~~~ Developed By RISHABH GUPTA ~~~
+      </p>
     </div>
   );
 }
